a338436c3413d5553a11b8e3bfbdd4325b8150aa
KartulUdus <derek.maekask@gmail.com>
2019-11-14 00:38:49 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="map-wrap" style="height: 100vh">
+    <client-only>
+      <l-map :zoom="13" :center="[59.413220, 24.219482]">
+        <l-tile-layer url="https://tiles.poracle.world/tile/klokantech-basic/{z}/{x}/{y}/2/png" />
+        <l-marker :lat-lng="[59.413220, 24.219482]" />
+      </l-map>
+    </client-only>
+  </div>
+</template>
+<script>
+export default {
+  mounted () {
+    console.log(this.$geolocation)
+  }
+}
+</script>
